02dcec871de114b7374b47025f88edda6ccb3505
sairam <talasilasairam@gmail.com>
2017-11-29 15:21:02 +0530

    Here Written the comments for Resource Bundles By T Sairam

--- DependsOn/src/com/dependson/test/DependsOnTest.java
+++ DependsOn/src/com/dependson/test/DependsOnTest.java
@@ -23,22 +23,25 @@ public class DependsOnTest {
 	//ApplicationContext applicationContext = new ClassPathXmlApplicationContext("");
 	LoanPlanner loadPlanner = factory.getBean("loanPlanner",LoanPlanner.class);
 	loadPlanner.calculate(10000,20,"delhi");
-	Locale locale = Locale.CHINESE;
+	Locale locale = Locale.getDefault();
 	/*Properties prop =System.getProperties();
 	for(Object prop1 : prop.keySet())
 	{
 		System.out.println(prop1+"----------"+prop.getProperty((String)prop1));	
 	}*/
 	System.out.println("Locale information  "+locale +"  Locale Language "+            locale.getLanguage() +"  Locale Country  "+locale.getCountry());
+	
+	//Here we are fetching Bundle of resources Means here we are giving Base Name i.e Messages
 	ResourceBundle resourceBundle = ResourceBundle.getBundle("com/dependson/common/Messages", locale);
 	System.out.println("zzzzzzzzzzzz"+resourceBundle.getClass().getName());
+	
+	
 	InputStream stream = DependsOnTest.class.getClassLoader().getResourceAsStream("com/dependson/common/Messages.properties");
-	System.out.println("Stream  "+stream);
+	
+	//Here it is expecting InputStream So we have to pass The Exact Location Of the File it is Implementation Class of ResourceBundle
 	PropertyResourceBundle prb = new PropertyResourceBundle(stream);
 	System.out.println("ProprtyResourceBundle  "+prb.getString("Appstek"));
-	System.out.println("Resource bundle object  "+resourceBundle.getLocale());
-	String value = resourceBundle.getString("Appstek");
-	System.out.println("value for the given key "+value);
+	System.out.println("value for the given key "+resourceBundle.getString("Appstek"));
 	/*for(Locale allLocale : Locale.getAvailableLocales()) {
 		System.out.println("Country Name "+allLocale.getDisplayCountry()+"  CountryLanguage  "+allLocale.getLanguage());
 	}*/
